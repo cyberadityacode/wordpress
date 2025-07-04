9c72b57ba87d06e4897bd40c3003dbd8e8c41ee0
Aditya Dubey <adityadubey793@gmail.com>
2025-07-04 14:31:41 +0530

    added form validation using jquery validate

---
+++ 37 Plugin Development 10 - Form Validation using jQuery validate/pages/list-project.php
@@ -0,0 +1,46 @@
+
+    <div class="container">
+        <h2>Bordered Table</h2>
+        <div class="container">
+            <h2>Panel Heading</h2>
+            <div class="panel panel-primary">
+                <div class="panel-heading">List Projects</div>
+                <div class="panel-body">
+                    <p>The .table-bordered class adds borders to a table:</p>
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <table class="table table-bordered" id="tbl-project">
+                                <thead>
+                                    <tr>
+                                        <th>#ID</th>
+                                        <th>#Project Name</th>
+                                        <th>#Client</th>
+                                        <th>#Technology</th>
+                                        <th>#Excerpt</th>
+                                        <th>#Options</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>1</td>
+                                        <td>Portfolio Theme WP</td>
+                                        <td>DC Aditya Dubey</td>
+                                        <td>WP,PHP,jQUERY</td>
+                                        <td>Dynamic Project Portfolio theme</td>
+                                        <td>
+                                            <a href="javascript:void(0)" class="btn btn-info">View</a>
+                                            <a href="javascript:void(0)" class="btn btn-warning">Edit</a>
+                                            <a href="javascript:void(0)" class="btn btn-danger">Delete</a>
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
---
+++ 37 Plugin Development 10 - Form Validation using jQuery validate/readme.md
@@ -0,0 +1,5 @@
+# Custom Plugin Development 10
+
+Form Validation using jQuery
+
+Download and enqueue jquery validate JS file and proceed herewith
